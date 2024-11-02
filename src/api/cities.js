5f40bb6155efd35da52e8c403704424587539a4e
M3fjuLeo <mateuszropek@onet.pl>
2024-11-02 20:03:16 +0100

    server

--- src/api/cities.js
+++ src/api/cities.js
@@ -1,13 +1,13 @@
+// api/cities.js
 import { createServer, Model } from "json-server";
 import { readFileSync } from "fs";
 
 const server = createServer();
 const data = JSON.parse(readFileSync("./data/cities.json", "utf-8"));
-server.use(Model());
-server.db = server.db;
-server.use(jsonServer.rewriter({}));
-server.use(jsonServer.defaults());
-server.db = { cities: data };
+
+server.db = {
+  cities: data,
+};
 
 server.listen(8000, () => {
   console.log("JSON Server is running");
